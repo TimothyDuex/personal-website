ebf69a3a3091d1f6dcf71a7d60f8e0fe3c6fda08
Timothy Duex <timothyduex@gmail.com>
2025-08-27 10:16:14 -0400

    Add skills section to about page

---
+++ src/app/_info/Skill.ts
@@ -0,0 +1,116 @@
+import {
+  NextJs,
+  React,
+  Redux,
+  ReactRouter,
+  TypeScript,
+  JavaScript,
+  Jest,
+  TailwindCSS,
+  HTML5,
+  CSS3,
+  Webpack,
+  ESLint,
+  Prettier,
+  Figma,
+  NodeJs,
+  Java,
+  EC2,
+  GraphQL,
+  PostgreSQL,
+  AWS,
+  Redis,
+  Supabase,
+  Postman,
+  Git,
+  NPM,
+} from "developer-icons";
+import { SVGProps } from "react";
+
+interface DeveloperIconProps extends Partial<SVGProps<SVGElement>> {
+  size?: number;
+}
+
+interface Skill {
+  name: string;
+  icon: React.FC<DeveloperIconProps>;
+  url: string;
+}
+
+export interface SkillCategory {
+  title: string;
+  skills: Skill[];
+}
+
+const skillCategories: SkillCategory[] = [
+  {
+    title: "Frontend",
+    skills: [
+      { name: "React", icon: React, url: "https://react.dev" },
+      { name: "Redux", icon: Redux, url: "https://redux.js.org" },
+      { name: "React Router", icon: ReactRouter, url: "https://redux.js.org" },
+      { name: "Next.js", icon: NextJs, url: "https://nextjs.org" },
+      {
+        name: "TypeScript",
+        icon: TypeScript,
+        url: "https://typescriptlang.org",
+      },
+      {
+        name: "JavaScript",
+        icon: JavaScript,
+        url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+      },
+      { name: "Jest", icon: Jest, url: "https://jestjs.io" },
+      {
+        name: "Tailwind CSS",
+        icon: TailwindCSS,
+        url: "https://tailwindcss.com",
+      },
+      {
+        name: "HTML5",
+        icon: HTML5,
+        url: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+      },
+      {
+        name: "CSS3",
+        icon: CSS3,
+        url: "https://developer.mozilla.org/en-US/docs/Web/CSS",
+      },
+      {
+        name: "Webpack",
+        icon: Webpack,
+        url: "https://webpack.js.org",
+      },
+      { name: "ESLint", icon: ESLint, url: "https://eslint.org" },
+      { name: "Prettier", icon: Prettier, url: "https://prettier.io" },
+      { name: "Figma", icon: Figma, url: "https://eslint.org" },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      { name: "Node.js", icon: NodeJs, url: "https://nodejs.org" },
+      { name: "Java", icon: Java, url: "https://oracle.com/java" },
+      { name: "EC2", icon: EC2, url: "https://aws.amazon.com/ec2/" },
+      { name: "GraphQL", icon: GraphQL, url: "https://graphql.org" },
+      { name: "PostgreSQL", icon: PostgreSQL, url: "https://postgresql.org" },
+      {
+        name: "DynamoDB",
+        icon: AWS,
+        url: "https://aws.amazon.com/dynamodb",
+      },
+      { name: "Redis", icon: Redis, url: "https://redis.io" },
+      { name: "Supabase", icon: Supabase, url: "https://supabase.com" },
+      { name: "AWS", icon: AWS, url: "https://aws.amazon.com" },
+      { name: "Postman", icon: Postman, url: "https://www.postman.com" },
+      { name: "Git", icon: Git, url: "https://github.com" },
+      { name: "NPM", icon: NPM, url: "https://www.npmjs.com" },
+    ],
+  },
+];
+
+function getSkills(): SkillCategory[] {
+  return skillCategories;
+}
+
+export default getSkills;
--- src/app/about/AboutSection.tsx
+++ src/app/about/AboutSection.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import FloatingIcon from "../_components/_animations/FloatingIcon";
+import SkillSection from "./SkillSection";
 
 const AboutSection = () => {
   return (
@@ -8,31 +9,34 @@ const AboutSection = () => {
         About Tim {<FloatingIcon duration={2}>✌</FloatingIcon>}
       </h2>
 
-      <div className="rounded-2xl mb-8">
-        {/* TODO: Add back once implemented caching solution for TTS */}
-        {/* <SmartListenableText className="prose max-w-none" as="div"> */}
-        <p className="text-lg text-secondary-text leading-relaxed mb-6">
-          I am a Software Engineer with a strong background in web development,
-          full-stack engineering, distributed systems, and micro-services. My
-          experience in software development encompasses various aspects of
-          engineering, and I thrive on solving complex problems and building
-          robust solutions.
-        </p>
+      <div className="grid sm:grid-cols-1 md:grid-cols-3 gap-3">
+        <div className="md:col-span-2 rounded-2xl mb-8">
+          {/* TODO: Add back once implemented caching solution for TTS */}
+          {/* <SmartListenableText className="prose max-w-none" as="div"> */}
+          <p className="text-lg text-secondary-text leading-relaxed mb-6">
+            I am a Software Engineer with a strong background in web
+            development, full-stack engineering, distributed systems, and
+            micro-services. My experience in software development encompasses
+            various aspects of engineering, and I thrive on solving complex
+            problems and building robust solutions.
+          </p>
 
-        <p className="text-lg text-secondary-text leading-relaxed mb-6">
-          Beyond my professional expertise, I have a deep passion for music and
-          music production. Combining my technical skills with my love for
-          music, I aim to explore innovative ways to integrate technology into
-          the music industry. I am dedicated to continuous learning and growth
-          in both the software development field and the world of music.
-        </p>
+          <p className="text-lg text-secondary-text leading-relaxed mb-6">
+            Beyond my professional expertise, I have a deep passion for music
+            and music production. Combining my technical skills with my love for
+            music, I aim to explore innovative ways to integrate technology into
+            the music industry. I am dedicated to continuous learning and growth
+            in both the software development field and the world of music.
+          </p>
 
-        <p className="text-lg text-secondary-text leading-relaxed mb-6">
-          Feel free to connect with me to discuss opportunities where I can
-          leverage my technical skills and passion for music to create impactful
-          and creative solutions.
-        </p>
-        {/* </SmartListenableText> */}
+          <p className="text-lg text-secondary-text leading-relaxed mb-6">
+            Feel free to connect with me to discuss opportunities where I can
+            leverage my technical skills and passion for music to create
+            impactful and creative solutions.
+          </p>
+          {/* </SmartListenableText> */}
+        </div>
+        <SkillSection />
       </div>
     </>
   );
