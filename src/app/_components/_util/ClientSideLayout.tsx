56f48ea049e0defff9db39341b882efe6313a06e
Timothy Duex <timothyduex@gmail.com>
2025-09-16 15:18:22 -0400

    Make text not transparent to the background vinyl

--- src/app/_components/_util/ClientSideLayout.tsx
+++ src/app/_components/_util/ClientSideLayout.tsx
@@ -18,7 +18,9 @@ const ClientSideLayout: React.FC<ClientSideLayoutProps> = ({ children }) => {
       <VinylBackground containerRef={containerRef} />
       <div className="max-w-4xl mx-6 my-6 px-6 py-4 glass-card-primary">
         {/* Respective Content to NavBar Selection */}
-        <div className="max-w-4xl mx-auto px-6 py-2">{children}</div>
+        <div className="max-w-4xl mx-auto px-6 py-2 z-1 relative">
+          {children}
+        </div>
       </div>
     </div>
   );
